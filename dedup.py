e2539941325882622ae50b01c7f9176d0786c41a
Jiajie Chen <c@jia.je>
2023-02-02 01:00:43 +0800

    Handle refunds

--- dedup.py
+++ dedup.py
@@ -8,7 +8,8 @@ def find_wechat_family(new_entries_list, existing_entries):
     wechat_family = defaultdict(list)
     for key, new_entries in new_entries_list:
         for entry in new_entries:
-            if entry.narration == "亲属卡" and entry.postings[1].account == "Expenses:Family":
+            if (entry.narration == "亲属卡" or entry.narration == "亲属卡-退款") \
+                    and entry.postings[1].account == "Expenses:Family":
                 wechat_family[(entry.date, entry.postings[0])].append(entry)
 
     # Collect corresponding transactions
@@ -23,15 +24,15 @@ def find_wechat_family(new_entries_list, existing_entries):
         mod_entries = []
         for entry in new_entries:
             # Find matching entry
-            if entry.narration == "亲属卡" and entry.postings[1].account == "Expenses:Family":
+            if (entry.narration == "亲属卡" or entry.narration == "亲属卡-退款") \
+                    and entry.postings[1].account == "Expenses:Family":
                 if (entry.date, entry.postings[0]) in corresponding:
                     marked_meta = entry.meta.copy()
                     marked_meta[DUPLICATE_META] = True
                     entry = entry._replace(meta=marked_meta)
             elif "财付通" in entry.narration or "微信支付" in entry.narration:
                 if (entry.date, entry.postings[0]) in wechat_family:
-                    tags = entry.tags.copy()
-                    tags = tags.union({"Family"})
+                    tags = entry.tags.union({"Family"})
                     entry = entry._replace(tags=tags)
             mod_entries.append(entry)
         mod_entries_list.append((key, mod_entries))
