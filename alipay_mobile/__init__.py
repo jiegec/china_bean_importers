c5c0cedd6a73034065205b75fcdbbe7114cf6a3b
Summer <flyalready@duck.com>
2024-07-28 11:56:54 +0800

    Improve credit spending and repayment transactions handling - Identify Huabei and Douyin Monthly Payment transactions - Record them in corresponding Liabilities account rather than Expenses

--- alipay_mobile/__init__.py
+++ alipay_mobile/__init__.py
@@ -108,7 +108,7 @@ class Importer(CsvImporter):
                 # TODO: handle 红包 & 余额宝转入
                 source_config = self.config["importers"]["alipay"]
                 account1 = source_config["account"]  # 支付宝余额
-                if method == "花呗":
+                if "花呗" in method: # Handle discount transactions like "花呗&红包"
                     account1 = source_config["huabei_account"]
                 if method == "余额宝":
                     account1 = source_config["yuebao_account"]
@@ -126,6 +126,11 @@ class Importer(CsvImporter):
                         if not expense
                         else source_config["red_packet_expense_account"]
                     )
+                elif expense and category == "信用借还":
+                    if "还款" in narration and "花呗" in payee:
+                        account2 = source_config["huabei_account"]
+                    elif "抖音月付" in narration:
+                        account2 = source_config["douyin_monthly_payment_account"]
                 else:
                     new_account, new_meta, new_tags = match_destination_and_metadata(
                         self.config, narration, payee
