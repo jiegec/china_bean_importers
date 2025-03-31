7b141de64a28fcce9da5ac51fb973f1a8c2b0784
Trim21 <trim21.me@gmail.com>
2025-03-31 10:25:21 +0800

    org files

---
+++ china_bean_importers/__init__.py
@@ -0,0 +1,29 @@
+from china_bean_importers import (
+    alipay_mobile,
+    alipay_web,
+    boc_credit_card,
+    boc_debit_card,
+    ccb_debit_card,
+    cmb_debit_card,
+    cmbc_credit_card,
+    cmbc_debit_card,
+    icbc_debit_card,
+    hsbc_hk,
+    thu_ecard_old,
+    wechat,
+)
+
+__all__ = [
+    "alipay_mobile",
+    "alipay_web",
+    "boc_credit_card",
+    "boc_debit_card",
+    "ccb_debit_card",
+    "cmb_debit_card",
+    "cmbc_credit_card",
+    "cmbc_debit_card",
+    "icbc_debit_card",
+    "hsbc_hk",
+    "thu_ecard_old",
+    "wechat",
+]
